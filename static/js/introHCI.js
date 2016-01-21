c03b1f64a9679bef21a9c263e341dfc6b94829ea
chaopan <chaopan0329@gmail.com>
2016-01-21 12:41:15 -0800

    finish

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,27 +8,50 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+
+ 
+
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
-
+    
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-	function initializePage(){
-		$("#testjs").click(function(e){
-			$('.junbotron h1').text("Javascript is connected");
-		
-		});
-		$("a.thumbnail").click(projectClick);
-	}
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject);
 }
 
 
 function projectClick(e) {
-    // prevent the page from reloading    
+  // Cancel the default action, which prevents the page from reloading
     e.preventDefault();
-    // In an event handler, $(this) refers to
-    // the object that triggered the event
-    $(this).css("background-color", "#7fff00");
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+	
+	
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+	}
+	
+	function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+	console.log("hello");
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
 }
+
+
